b2c515758a3c3e9a04fe84abc994e092d6180447
Kia Wright <kiawright@Kias-MBP.attlocal.net>
2022-09-28 11:34:02 -0500

    changed phone authentication to expo firebase

--- screens/PhoneNumber.js
+++ screens/PhoneNumber.js
@@ -1,21 +1,48 @@
 import React, { useRef, useState } from 'react';
-import { Button, Pressable, SafeAreaView, Text, TextInput, View } from 'react-native';
+import { Button, Platform, Pressable, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import Styles from "../style.js";
 import Ionicons from '@expo/vector-icons/Ionicons';
 
 import PhoneInput, {isValidNumber} from "react-native-phone-number-input";
 import { sendSmsVerification } from "../api/verify";
 
-function NewUser({ navigation }) {
-    const [phone, setPhone] = useState('');
-    const [formattedValue, setFormattedValue] = useState('');
+import { FirebaseRecaptchaVerifierModal, FirebaseRecaptchaBanner } from 'expo-firebase-recaptcha';
+import { initializeApp, getApp } from 'firebase/app';
+import { getAuth, PhoneAuthProvider, signInWithCredential } from 'firebase/auth';
 
-    // https://www.twilio.com/blog/phone-verification-react-native
-    const sendCode = () => {
-        sendSmsVerification(formattedValue).then((sent) => {
-            navigation.navigate("Otp", { phoneNumber: formattedValue });
-          });
-    };
+
+function NewUser({ navigation }) {
+    
+    // Firebase references
+const app = getApp();
+const auth = getAuth(app);
+
+// Double-check that we can run the example
+if (!app?.options || Platform.OS === 'web') {
+  throw new Error(
+    'This example only works on Android or iOS, and requires a valid Firebase config.'
+  );
+}
+    // Ref or state management hooks
+  const recaptchaVerifier = React.useRef(null);
+  const [phoneNumber, setPhoneNumber] = React.useState();
+  const [verificationId, setVerificationId] = React.useState();
+  const [verificationCode, setVerificationCode] = React.useState();
+
+  const firebaseConfig = app ? app.options : undefined;
+  const [message, showMessage] = React.useState();
+  const attemptInvisibleVerification = false;
+
+
+    // const [phone, setPhone] = useState('');
+    // const [formattedValue, setFormattedValue] = useState('');
+
+    // // https://www.twilio.com/blog/phone-verification-react-native
+    // const sendCode = () => {
+    //     sendSmsVerification(formattedValue).then((sent) => {
+    //         navigation.navigate("Otp", { phoneNumber: formattedValue });
+    //       });
+    // };
 
     return (
         <SafeAreaView style={[Styles.centerContainer, Styles.fullScreen]}>
@@ -33,7 +60,7 @@ function NewUser({ navigation }) {
                     </Text>
                 </View>*/}
 
-                <View style={{marginBottom: 20, alignSelf: 'center'}}>
+                {/* <View style={{marginBottom: 20, alignSelf: 'center'}}>
                     <View style={{alignSelf: 'center'}}>
                         <Text style={[Styles.heading1, {fontFamily: 'Poppins_600SemiBold', marginBottom: 20}]}>Phone number</Text>
                         
@@ -55,7 +82,87 @@ function NewUser({ navigation }) {
                                 <Text style={Styles.buttonText}>Send code</Text>
                         </Pressable>
                     </View>
-                </View>
+                </View> */}
+
+<View style={{ padding: 20, marginTop: 50 }}>
+      <FirebaseRecaptchaVerifierModal
+        ref={recaptchaVerifier}
+        firebaseConfig={app.options}
+        // attemptInvisibleVerification
+      />
+      <Text style={{ marginTop: 20 }}>Enter phone number</Text>
+      <TextInput
+        style={{ marginVertical: 10, fontSize: 17 }}
+        placeholder="+1 999 999 9999"
+        autoFocus
+        autoCompleteType="tel"
+        keyboardType="phone-pad"
+        textContentType="telephoneNumber"
+        onChangeText={phoneNumber => setPhoneNumber(phoneNumber)}
+      />
+      <Button
+        title="Send Verification Code"
+        disabled={!phoneNumber}
+        onPress={async () => {
+          // The FirebaseRecaptchaVerifierModal ref implements the
+          // FirebaseAuthApplicationVerifier interface and can be
+          // passed directly to `verifyPhoneNumber`.
+          debugger
+          try {
+            const phoneProvider = new PhoneAuthProvider(auth);
+            const verificationId = await phoneProvider.verifyPhoneNumber(
+              phoneNumber,
+              recaptchaVerifier.current
+            );
+            setVerificationId(verificationId);
+            showMessage({
+              text: 'Verification code has been sent to your phone.',
+            });
+          } catch (err) {
+            showMessage({ text: `Error: ${err.message}`, color: 'red' });
+          }
+        }}
+      />
+      <Text style={{ marginTop: 20 }}>Enter Verification code</Text>
+      <TextInput
+        style={{ marginVertical: 10, fontSize: 17 }}
+        editable={!!verificationId}
+        placeholder="123456"
+        onChangeText={setVerificationCode}
+      />
+      <Button
+        title="Confirm Verification Code"
+        disabled={!verificationId}
+        onPress={async () => {
+          try {
+            const credential = PhoneAuthProvider.credential(verificationId, verificationCode);
+            await signInWithCredential(auth, credential);
+            showMessage({ text: 'Phone authentication successful 👍' });
+          } catch (err) {
+            showMessage({ text: `Error: ${err.message}`, color: 'red' });
+          }
+        }}
+      />
+      {message ? (
+        <TouchableOpacity
+          style={[
+            StyleSheet.absoluteFill,
+            { backgroundColor: 0xffffffee, justifyContent: 'center' },
+          ]}
+          onPress={() => showMessage(undefined)}>
+          <Text
+            style={{
+              color: message.color || 'blue',
+              fontSize: 17,
+              textAlign: 'center',
+              margin: 20,
+            }}>
+            {message.text}
+          </Text>
+        </TouchableOpacity>
+      ) : undefined}
+      {attemptInvisibleVerification && <FirebaseRecaptchaBanner />}
+    </View>
             </View>
          </SafeAreaView>
     );
